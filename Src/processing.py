4ab3a19344956736ee6bed57aa2c9ecb5a40b4f5
Jaroslav <Jt999@bk.ru>
2025-04-08 01:00:59 +0300

    'processing+'

--- Src/processing.py
+++ Src/processing.py
@@ -21,3 +21,28 @@ print(executed_transactions)
 # Вызов функции с передачей статуса 'CANCELED'
 canceled_transactions = filter_by_state(data, "CANCELED")
 print(canceled_transactions)
+
+
+def sort_by_date(transactions, descending=True):
+    # Мы создаем новый список, куда будем добавлять отсортированные элементы
+    sorted_transactions = []
+
+    if descending:
+        sorted_transactions = sorted(transactions, key=lambda x:['date'],reverse=True)
+    else:
+        sorted_transactions = sorted(transactions, key=lambda x:['date'])
+
+    return sorted_transactions
+
+
+# Пример входных данных
+transactions = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+# Демонстрация работы функции
+sorted_transactions_desc = sort_by_date(transactions)
+print(sorted_transactions_desc)
