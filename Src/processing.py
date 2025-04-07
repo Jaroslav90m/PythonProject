ff479cece66a3c0c463600c7c86dfeeb074f0e07
Jaroslav <Jt999@bk.ru>
2025-04-08 01:00:16 +0300

    'processing+'

--- Src/processing.py
+++ Src/processing.py
@@ -1,16 +1,17 @@
-def filter_by_state(data, state='EXECUTED'):
+def filter_by_state(data, state="EXECUTED"):
     result = []  # Создаем пустой список для хранения результатов
     for item in data:
-        if item['state'] == state:
+        if item["state"] == state:
             result.append(item)
     return result
 
+
 # Пример входных данных для проверки функции
 data = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
 # Вызов функции с использованием статуса по умолчанию 'EXECUTED'
@@ -18,5 +19,5 @@ executed_transactions = filter_by_state(data)
 print(executed_transactions)
 
 # Вызов функции с передачей статуса 'CANCELED'
-canceled_transactions = filter_by_state(data, 'CANCELED')
+canceled_transactions = filter_by_state(data, "CANCELED")
 print(canceled_transactions)
